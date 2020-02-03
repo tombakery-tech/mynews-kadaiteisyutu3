548a02b5403294311eab4ac80aeec87afaba0107
kato <itizisiyouitasimasu@hotmail.co.jp>
2020-02-03 05:04:34 +0000

    課題提出14

---
+++ app/Http/Controllers/Admin/NewsController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Controllers\admin;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+use App\News;
+
+class NewsController extends Controller
+{
+    public function add()
+    {
+        return view('admin.news.create');
+    }
+    
+    public function create(Request $request)
+{
+    $this->validate($request, News::$rules);
+    
+    $news = new News;
+    $form = $request->all();
+    
+    if(isset($form['image'])) {
+        $path = $request->file('image')->store('public/image');
+        $news->image_path = basename($path);
+        } else {
+            $news->image_path = null;
+        }
+        
+        unset($form['_token']);
+        unset($form['image']);
+        
+        $news->fill($form);
+        $news->save();
+        
+    // admin/news/createにリダイレクト
+    return redirect('admin/news/create');
+ }
+}
+
+//あとで消す
---
+++ app/Http/Controllers/Admin/ProfileController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Http\Controllers\admin;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+use App\Profile;
+
+class ProfileController extends Controller
+{
+    
+    public function add()
+    {
+        return view('admin.profile.create');
+    }
+    
+    public function create(Request $request)
+    {
+     $this->validate($request, Profile::$rules);
+    
+    $profile = new Profile;
+    $form = $request->all();
+    
+        unset($form['_token']); 
+        
+        $profile->fill($form);
+        $profile->save();
+        
+        return redirect('admin/profile/create');
+    }
+    
+    public function edit()
+    {
+        return view('admin.profile.edit');
+    }
+    
+    public function update()
+    {
+         return redirect('admin/profile/edit');
+    }
+    
+   
+}
+
