51f3b793f2318dc224241b4b3fae14f24ffc1db1
kato <itizisiyouitasimasu@hotmail.co.jp>
2020-02-03 13:05:07 +0000

    課題提出14

--- app/Http/Controllers/Admin/NewsController.php
+++ app/Http/Controllers/Admin/NewsController.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Http\Controllers\admin;
+namespace App\Http\Controllers\Admin;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
@@ -37,6 +37,47 @@ class NewsController extends Controller
     // admin/news/createにリダイレクト
     return redirect('admin/news/create');
  }
+
+//コース15にて追記
+public function index(Request $request)
+{
+    $cond_title = $request->cond_title;
+    if ($cond_title != '') {
+        $posts = News::where('title', $cond_title)->get();
+    } else {
+        $posts = News::all();
+    }
+    return view('admin.news.index', ['posts' => $posts, 'cond_title' => $cond_title]);
 }
 
+//コース16にて追記
+public function edit(Request $request)
+{
+    $news = News::find($request->id);
+    
+    return view('admin.news.edit', ['news_form' => $news]);
+}
+
+public function update(Request $request)
+{
+    $this->validate($request, News::$rules);
+    
+    // News Modelからデータを取得する
+      $news = News::find($request->id);
+      // 送信されてきたフォームデータを格納する
+      $news_form = $request->all();
+      unset($news_form['_token']);
+
+     $news->fill($news_form)->save();
+    
+    return redirect('admin/news/');
+}
+
+public function delete(Request $request)
+  {
+      $news = News::find($request->id);
+      $news->delete();
+      return redirect('admin/news/');
+  }  
+}
 //あとで消す
--- routes/web.php
+++ routes/web.php
@@ -18,6 +18,10 @@ Route::get('/', function () {
 Route::group(['prefix' => 'admin', 'middleware' => 'auth'], function() {
     Route::get('news/create', 'Admin\NewsController@add');
     Route::post('news/create', 'Admin\NewsController@create');
+    Route::get('news', 'Admin\NewsController@index');
+    Route::get('news/edit', 'Admin\NewsController@edit');
+    Route::post('news/edit', 'Admin\NewsController@update');
+    Route::get('news/delete', 'Admin\NewsController@delete');
     Route::get('profile/create', 'Admin\ProfileController@add');
     Route::post('profile/create', 'Admin\ProfileController@create');
     Route::get('profile/edit', 'Admin\ProfileController@edit');
@@ -42,3 +46,4 @@ Route::get('/home', 'HomeController@index')->name('home');
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
